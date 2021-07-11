86fddc8f8878bb89851593eebed75bf58706b931
Ikram Abidat <40496953+ikrami11@users.noreply.github.com>
2021-07-11 11:08:34 +0100

    applying styles

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
-    state = {  }
-    render() { 
-        return <h1>Hello world!</h1>
+    state = { 
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
+     };
+
+render() { 
+
+        return (<div>
+                    <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                    <button className="btn btn-secondary btn-sm">Increment</button>
+                    <ul>
+                        {this.state.tags.map( tag => <li key={tag}>{tag}</li>)}
+                    </ul>
+                </div>
+            );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state; //picking up the count from the state in a const
+        return count === 0 ? 'Zero' : count; //if count = 0 return 'zero', otherwise return count
     }
 }
  
